f3d68992725a873fd05a456de0a2e5d8be840688
msx80 <msx80@github>
2019-03-17 12:02:59 +0100

    cleaning

--- baremetaltic80/kernel.cpp
+++ baremetaltic80/kernel.cpp
@@ -27,17 +27,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <circle/usb/usbkeyboard.h>
+#include "utils.h"
+#include <limits.h>
 
 static tic80_input tic_input;
-
-#define MAXBUFLEN (1024*1024)
-
-
 
 CKernel::CKernel (void)
 :	CStdlibAppStdio ("Tic-80")
 {
-	mActLED.Blink (5);	// show we are alive
 }
 
 void screenCopy(CScreenDevice* screen, tic80* tic)
@@ -62,50 +59,11 @@ void screenCopy(CScreenDevice* screen, tic80* tic)
 //	memcpy(screen->GetBuffer(), tic->screen, 240*136*4);
 }
 
-void* loadFile(char* filename, u32* size)
-{
-  char source[MAXBUFLEN];
-  FILE *fp = fopen(filename, "rb");
-  if (fp != NULL) {
-    size_t newLen = fread(source, sizeof(char), MAXBUFLEN, fp);
-    if ( ferror( fp ) != 0 ) {
-        fputs("Error reading file", stderr);
-    } else {
-       // source[newLen++] = '\0'; /* Just to be safe. */
-    }
-
-    fclose(fp);
-    printf("STREAM SIZE is %d\n", newLen);
-    void* cart = malloc(newLen);
-    memcpy(cart, source, newLen);
-    *size = newLen;
-    return cart;
-  }
-  else
-  {
-    return 0;
-  }
-}
-
-void CKernel::KeyPressedHandler (const char *pString)
-{
-	int len = strlen(pString);
-	//printf("SIZE OF STRING: %d\n", strlen(pString));
-	//printf(pString);
-	if (len==1)
-	{
-	
-		tic_input.gamepads.first.a = true;
-	}
-	
-}
 void CKernel::KeyStatusHandlerRaw (unsigned char ucModifiers, const unsigned char RawKeys[6])
 {
 
 	tic_input.gamepads.first.data = 0;
-	
 
-//	printf("MODIF %02x ", ucModifiers);
 	for (unsigned i = 0; i < 6; i++)
 	{
 		if (RawKeys[i] != 0)
@@ -122,27 +80,25 @@ void CKernel::KeyStatusHandlerRaw (unsigned char ucModifiers, const unsigned cha
 
 		}
 	}
-
-//	printf("\n");
 }
+CStdlibApp::TShutdownMode CKernel::Die(const char *msg)
+{
+	printf("FATAL\n");
+	printf(msg);
+	CTimer::SimpleMsDelay(100000);
+	return ShutdownHalt;
+}
+
 CStdlibApp::TShutdownMode CKernel::Run (void)
 {
-	mLogger.Write (GetKernelName (), LogNotice, "TIC80 Port test");
-
-	mLogger.Write (GetKernelName (), LogNotice, "stdio test...");
-
+	mLogger.Write (GetKernelName (), LogNotice, "TIC80 Port");
 
 	CUSBKeyboardDevice *pKeyboard = (CUSBKeyboardDevice *) mDeviceNameService.GetDevice ("ukbd1", FALSE);
 	if (pKeyboard == 0)
 	{
-		mLogger.Write (GetKernelName(), LogError, "Keyboard not found");
-
-		return ShutdownHalt;
+		return Die("Keyboard not found");
 	}
-	pKeyboard->RegisterKeyStatusHandlerRaw (KeyStatusHandlerRaw);
 
-	printf("Hello world!\n");
-	fprintf(stderr, "Hello world on stderr!\n");
 
 	CScreenDevice* screen = &mScreen;
 	printf("Screen is:\n");
@@ -151,77 +107,37 @@ CStdlibApp::TShutdownMode CKernel::Run (void)
 	printf("Creating tic80 instance..\n");
 
 	tic80* tic = tic80_create(44100);
+	if( !tic)
+	{
+		return Die("Could not tic80_create");
+	}
 	printf("tic80: %p\n", tic);
-	printf("screen: %p\n", tic->screen);
-
 	printf("Loading cart.tic...\n");
 
 	u32 size;
         void* cart = loadFile("cart.tic", &size);
 
-	        if(cart)
-	        {
-			tic80_load(tic, cart, size);
-		}
-		else
-		{
-			printf("no cart\n");
-		}
-
-	printf("screen: %p\n", tic->screen);
-	printf("screen: %p\n", tic->screen);
-
-		screen->GetBuffer()[0] = 123456789;
-		screen->GetBuffer()[239] = 13456789;
-		screen->GetBuffer()[135*240+239] = 13456789;
-
-
-	CTimer::SimpleMsDelay(5000);
-
-	
-	for(int i=0;i<10000;i++)
-	{
-		//printf("tic...");
-		tic80_tick(tic, tic_input);
-		screenCopy(screen, tic);
-
-		screen->GetBuffer()[0] = 123456789;
-		
-		screen->vsync();
-	}
-
-	printf("Type some characters and hit <RETURN>\n");
-
-	char line[200];
-	if (fgets(line, sizeof(line), stdin) != nullptr)
-	{
-		printf("Read '%s' from stdin...\n", line);
+        if(cart)
+        {
+		tic80_load(tic, cart, size);
 	}
 	else
 	{
-		perror("fgets returned NULL");
+		return Die("Could not load cart");
 	}
 
-	char outstr[200];
-        time_t const t = time (nullptr);
-        struct tm * const tmp = localtime (&t);
 
-        if (tmp == nullptr)
-        {
-                perror("localtime");
-        }
-        else if (strftime(outstr, sizeof(outstr), "%D %T", tmp) == 0)
-        {
-                fprintf(stderr, "strftime returned 0");
-        }
-        else
-        {
-                // Do not expect a valid current time as the Raspberry Pi
-                // clock has not been set.
-                printf("The current date/time is \"%s\"\n", outstr);
-        }
+	// CTimer::SimpleMsDelay(5000);
 
-	mLogger.Write (GetKernelName (), LogNotice, "C Standard Library Demo finished");
+	pKeyboard->RegisterKeyStatusHandlerRaw (KeyStatusHandlerRaw);
+
+
+	while(true)
+	{
+		tic80_tick(tic, tic_input);
+		screenCopy(screen, tic);
+		screen->vsync();
+	}
 
 	return ShutdownHalt;
 }
--- baremetaltic80/kernel.h
+++ baremetaltic80/kernel.h
@@ -28,6 +28,7 @@ public:
 	CKernel (void);
 	static void KeyPressedHandler (const char *pString);
 	static void KeyStatusHandlerRaw (unsigned char ucModifiers, const unsigned char RawKeys[6]);
+	TShutdownMode Die(const char *msg);
 	TShutdownMode Run (void);
 };
 
